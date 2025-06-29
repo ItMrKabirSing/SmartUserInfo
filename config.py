fd90333c4fad25598015372004e9324402c22fc5
ItMrKabirSing <ismartprogrammer73838@gmail.com>
2025-06-29 20:28:30 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 #Copyright @ISmartCoder
 #Updates Channel: https://t.me/TheSmartDev
 # Telegram API credentials
-API_ID = your_api_id
-API_HASH = "your_api_hash"
-BOT_TOKEN = "Your_Bot_token"
+API_ID = 26512884
+API_HASH = "c3f491cd59af263cfc249d3f93342ef8"
+BOT_TOKEN = "7582815223:AAFQZGv15dT_-fV2cD-c2v5q0RA2IorTmEw"
